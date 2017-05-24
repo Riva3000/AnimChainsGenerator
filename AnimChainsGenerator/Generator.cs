6c65fe3de4f07d0953702e07a0640d2abc34c8ec
Riva3000 <microriva@gmail.com>
2017-05-24 03:16:16 +0200

    Removed unused references

--- AnimChainsGenerator/Generator.cs
+++ AnimChainsGenerator/Generator.cs
@@ -1,10 +1,6 @@
 ﻿using FlatRedBall.Content.AnimationChain;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AnimChainsGenerator
 {
